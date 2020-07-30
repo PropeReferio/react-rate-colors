59a52d10c58b76a1f945464a63d59a284fafbf3a
PropeReferio <lemuel.b.stevens@gmail.com>
2020-07-30 00:40:59 -0500

    Before context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React, { useState } from 'react';
 import colorData from "./data/color-data.json";
 import ColorList from './components/ColorList.js';
+import AddColorForm from './components/AddColorForm';
+import { v4 } from 'uuid';
 import './App.css';
 
 export default function App() {
   const [colors, setColors] = useState(colorData);
   return (
-    <ColorList 
-      colors={colors}
-      onRateStar={(id, rating) => {
-        const newColors = colors.map(color =>
-          color.id === id ? { ...color, rating } : color
-          );
+    <>
+      <AddColorForm
+        onNewColor={(title, color) => {
+          const newColors = [
+            ...colors,
+            {
+              id: v4(),
+              rating: 0,
+              title,
+              color
+            }
+          ];
           setColors(newColors);
-      }}
-      onRemoveColor={id => {
-        const newColors = colors.filter(color => color.id !== id);
-        setColors(newColors);  // Makes a copy of colors, using filter, that does not include
-        // the color with the ID of the color to be removed, and sets that as the state var colors.
-      }} 
-    />
+        }}
+      />
+      <ColorList 
+        colors={colors}
+        onRateStar={(id, rating) => {
+          const newColors = colors.map(color =>
+            color.id === id ? { ...color, rating } : color
+            );
+            setColors(newColors);
+        }}
+        onRemoveColor={id => {
+          const newColors = colors.filter(color => color.id !== id);
+          setColors(newColors);  // Makes a copy of colors, using filter, that does not include
+          // the color with the ID of the color to be removed, and sets that as the state var colors.
+        }} 
+      />
+    </>
   );
 }
